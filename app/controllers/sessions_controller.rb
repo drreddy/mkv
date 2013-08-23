061c0bff8f568184f6e6b884e0772db37871fa0c
Rajeev Reddy <rajeev.reddy.d@gmail.com>
2013-08-23 12:23:44 -0400

    index changed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 	def create
   		auth = request.env["omniauth.auth"]
           
-        redirect_to root_url, :notice => "Signed in!"
+        redirect_to root_url, :notice => auth['info']['name']
 	end
     
     def new
