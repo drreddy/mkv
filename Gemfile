03da630fce94f5e3a23ff2429f92d762feb29bb7
Rajeev Reddy <rajeev.reddy.d@gmail.com>
2013-08-24 09:52:48 -0400

    posting using koala

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.12'
 
-ruby '1.9.3'
+# ruby '1.9.3'
 
 group :development, :test do
 	gem 'mysql2'
@@ -21,3 +21,5 @@ end
 gem 'jquery-rails'
 
 gem 'omniauth-facebook'
+
+gem 'koala'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,9 +6,8 @@ class HomeController < ApplicationController
   end
   
   def share
-    client = OAuth2::Client.new(Mkv::Application.config.app_id, Mkv::Application.config.app_secret, :site => 'https://graph.facebook.com')
-    token = OAuth2::AccessToken.new(client, session['fb_access_token'])
-    token.post('/me/feed', :message => "Mkv app", :link => "http://mkv.herokuapp.com", :name => "Mkv")
+    @graph = Koala::Facebook::API.new(session['fb_access_token'])
+    @graph.put_connections("me", "feed", :message => "Mkv app", :link => "http://mkv.herokuapp.com", :name => "Mkv", :description => "A social app integrating Facebook and Netflix")
     redirect_to root_url, :notice => 'Shared'
   end
 end
